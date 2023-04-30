42ccbe40aba5670ab44bda1eb1fef67c08b083b6
huulong <n.huu.long@gmail.com>
2023-04-30 23:38:47 +0200

    [Menu] Disable buttons, not viewport input, during main menu transitions, to avoid input being disabled later
    in level, as changing scene will interrupt the async code before reaching the line that re-enables viewport input.
    
    This is allow us to use the Pause Menu.

--- scripts/menu/MainMenuManager.gd
+++ scripts/menu/MainMenuManager.gd
@@ -4,6 +4,9 @@ extends Node
 ## Scene loaded when starting the game
 @export var first_scene: PackedScene
 
+## Main menu
+@export var main_menu: MainMenu
+
 ## Speed factor for initial fade in animation
 @export var initial_fade_in_speed: float = 1.0
 
@@ -16,20 +19,20 @@ extends Node
 
 func _ready():
 	assert(first_scene != null, "[MainMenuManager] first_scene is not set on %s" % get_path())
+	assert(main_menu != null, "[MainMenuManager] main_menu is not set on %s" % get_path())
 
-	# Disable input during fade in
-	_set_disable_input(true)
+	# Wait 1 frame so children are ready
+	await get_tree().process_frame
+
+	# Disable all buttons while fading in
+	main_menu.disable_all_buttons()
 	await TransitionScreen.fade_in_async(initial_fade_in_speed)
-	_set_disable_input(false)
+	main_menu.enable_all_buttons()
 
 
 func _on_main_menu_start_game_pressed():
-	# Disable input during transition
-	_set_disable_input(true)
+	# Disable all buttons before starting fading in for safety
+	main_menu.disable_all_buttons()
+
 	await SceneManager.change_scene_with_fade_async(first_scene,
 		start_game_fade_out_speed, start_game_fade_in_speed)
-	_set_disable_input(false)
-
-
-func _set_disable_input(value: bool):
-	get_tree().get_root().set_disable_input(value)
