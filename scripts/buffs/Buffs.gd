ab6c5d0ec89e79dd3458ab44b818cc4a7523b2a9
r0nk <r0nk@simplecpu.com>
2023-04-30 13:57:44 -0500

    fixed array removal bug

--- scripts/buffs/Buffs.gd
+++ scripts/buffs/Buffs.gd
@@ -4,9 +4,6 @@ extends Node
 #currently active
 var buff_stack = []
 
-func _ready ():
-	pass
-
 func get_attribute(name,baseline):
 	for buff in buff_stack:
 		if buff.attribute == name:
@@ -17,25 +14,15 @@ func get_attribute(name,baseline):
 					baseline+=buff.value
 	return baseline
 
-
-#returns true if the buff passed its probability test and was added.
 func add_if_roll(buff: Buff):
 	if randf()<buff.probability:
 		buff_stack.append(buff)
-		#TODO handle chain effects here
-		return true
-	return false
 
 func add(buff: Buff):
 	add_if_roll(buff)
 
 func _physics_process(delta):
-	for i in len(buff_stack):
-		if i>len(buff_stack): #FIXME wtf godot
-			continue
+	for i in range(buff_stack.size() -1 ,-1 ,-1):
 		buff_stack[i].duration-=delta
 		if buff_stack[i].duration<0:
-			#TODO handle detonation effects here
 			buff_stack.remove_at(i)
-
-
