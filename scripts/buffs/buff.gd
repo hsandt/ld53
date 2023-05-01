5d8d8509a732f17d763d37e5d9776e8d7a569a7c
r0nk <r0nk@simplecpu.com>
2023-05-01 10:30:17 -0500

    spark button press

--- scripts/buffs/buff.gd
+++ scripts/buffs/buff.gd
@@ -5,6 +5,10 @@ extends Control
 func _on_duration_timeout():
 	queue_free()
 
-func _on_spark():
+func _on_spark_button_pressed():
+	var buffs = get_parent()
 	if randf()<0.5:
-		pass
+		buffs.add(modifier.lucky)
+	else:
+		buffs.add(modifier.worsen)
+
