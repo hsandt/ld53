ea19cb50d82c6a115769e4c4e913dd94a05d81e5
r0nk <r0nk@simplecpu.com>
2023-05-01 17:50:05 -0500

    buffs now work with ui elements, spaghetti code

--- scripts/buffs/Buffs.gd
+++ scripts/buffs/Buffs.gd
@@ -3,9 +3,33 @@ extends Node2D
 
 @onready var buff_scene = preload("res://scripts/buffs/buff.tscn")
 
+
+#@onready var base_powder= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HboxContainer")
+@onready var base_powder= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer")
+@onready var powderpanel1= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel1")
+@onready var powderpanel2= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel2")
+@onready var powderpanel3= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel3")
+@onready var powderpanel4= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel4")
+@onready var powderpanel5= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel5")
+@onready var powderpanel6= get_node("/root/Node2D2/CanvasLayer/HUD/powderpanel/HBoxContainer/Powder Panel6")
+
+var bcount=0
+
+var buffs = []
+func _ready():
+	print(base_powder)
+	buffs.append(powderpanel1.get_node("buff"))
+	buffs.append(powderpanel2.get_node("buff"))
+	buffs.append(powderpanel3.get_node("buff"))
+	buffs.append(powderpanel4.get_node("buff"))
+	buffs.append(powderpanel5.get_node("buff"))
+	buffs.append(powderpanel6.get_node("buff"))
+
 #currently active
 func get_attribute(name,baseline):
-	for buff in get_children():
+	for buff in buffs:
+		if(buff.modifier == null):
+			continue
 		if buff.modifier.attribute == name:
 			match buff.modifier.effect:
 				"multiplier":
@@ -16,11 +40,8 @@ func get_attribute(name,baseline):
 
 func add_if_roll(modifier: Modifier):
 	if randf()<modifier.probability:
-		var b = buff_scene.instantiate()
-		b.modifier=modifier
-		b.position.y-=200+(100*len(get_children()))
-		b.get_node("progress/duration").wait_time=b.modifier.duration
-		add_child(b)
+		buffs[bcount].activate(modifier)
+		bcount+=1
 
 func add(modifier: Modifier):
 	add_if_roll(modifier)
