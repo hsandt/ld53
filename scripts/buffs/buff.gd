78e1a779d58de6b44ee0737729b535a1ff2e729d
r0nk <r0nk@simplecpu.com>
2023-05-01 10:47:32 -0500

    buff sfx

--- scripts/buffs/buff.gd
+++ scripts/buffs/buff.gd
@@ -6,9 +6,12 @@ func _on_duration_timeout():
 	queue_free()
 
 func _on_spark_button_pressed():
+	$spark_button/ui_click_sfx.play()
 	var buffs = get_parent()
 	if randf()<0.5:
+		$buff_sfx.play()
 		buffs.add(modifier.lucky)
 	else:
+		$debuff_sfx.play()
 		buffs.add(modifier.worsen)
 
