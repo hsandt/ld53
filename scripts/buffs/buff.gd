80208b37110b81a3c61147b39b7b10132ded97b9
huulong <n.huu.long@gmail.com>
2023-05-01 18:31:06 +0200

    [Buff] If there is no sub-modifier, do not show button to avoid error on click

--- scripts/buffs/buff.gd
+++ scripts/buffs/buff.gd
@@ -1,6 +1,15 @@
 extends Control
 
 @export var modifier: Modifier
+@onready var spark_button: Button = $spark_button
+
+func _ready():
+	assert(modifier != null,
+		"[buff] on buff '%s', modifier is not defined" % get_path())
+
+	if modifier.lucky == null and modifier.worsen == null:
+		# no further modifier, no button
+		spark_button.hide()
 
 func _on_duration_timeout():
 	queue_free()
@@ -8,7 +17,24 @@ func _on_duration_timeout():
 func _on_spark_button_pressed():
 	$spark_button/ui_click_sfx.play()
 	var buffs = get_parent()
-	if randf()<0.5:
+
+	if modifier.lucky == null and modifier.worsen == null:
+		push_error("[buff] button should not even be visible")
+		return
+
+	var trigger_lucky_effect: bool
+
+	if modifier.lucky == null:
+		# only worsen, so trigger worsen
+		trigger_lucky_effect = false
+	elif modifier.worsen == null:
+		# only lucky, so trigger lucky
+		trigger_lucky_effect = true
+	else:
+		# both exist, so it's random
+		trigger_lucky_effect = randf() < 0.5
+
+	if trigger_lucky_effect:
 		$buff_sfx.play()
 		buffs.add(modifier.lucky)
 	else:
