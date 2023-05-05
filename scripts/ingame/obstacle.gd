5b420235e147f1700af26038e483a04c713a5353
huulong <n.huu.long@gmail.com>
2023-05-05 18:46:10 +0200

    [Data] Change cargo powders to have only once steer_speed modifier, and extract obstacle data to resources

--- scripts/ingame/obstacle.gd
+++ scripts/ingame/obstacle.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
-@export var damage = 10
+
+@export var data: ObstacleData
+
+
+func _ready():
+	assert(data != null,
+		"[Obstacle] data is not set on %s" % get_path())
+
 
 func _on_body_entered(body):
 	if body.has_method("hurt"):
-		body.hurt(damage)
+		body.hurt(data.damage)
 
