30f57c47a01fdd3739201926c6ed9f5065a0a866
huulong <n.huu.long@gmail.com>
2023-05-05 17:02:34 +0200

    [Credits] Focus Back button on open, allow scrolling with ui_(page)up/down with tweening
    Note that we had to make the Rich Text Label Fit Content so the parent ScrollContainer could recognize its size and actually show its scrollbar, so we can set it by code. The scrollbar we saw so far was the integrated scrollbar of the text, which was unrelated. For safety we also disabled Scroll Active, but the text's own scrollbar won't show as long as the text is contained in some ScrollContainer anyway.

--- scripts/menu/credits_menu_controller.gd
+++ scripts/menu/credits_menu_controller.gd
@@ -9,19 +9,50 @@ signal close
 ## Credits rich text label, dynamically filled with credits_file_path
 @export var credits_label: RichTextLabel
 
+# Inspired by follow_focus_center.gd
+@export var animate = true
+@export var transition_time = 0.2
+@export var scroll_increment: float = 40
+@export var scroll_page_increment: float = 120
+
+@onready var scroll_container: ScrollContainer = %ScrollContainer
+@onready var back_button: Button = %BackButton
+
 
 func _ready():
 	assert(credits_label != null, "[credits_menu_controller] credits_label is not set on %s" % get_path())
 
 	var text := load_text()
-	credits_label.text = text
+	credits_label.text = text.strip_edges(false, true)
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("ui_cancel") && visible:
+	# Safety check
+	if not visible:
+		return
+
+	if event.is_action_pressed("ui_cancel"):
 		accept_event()
 		go_back()
+		return
 
+	var scroll_delta: float = 0.0
+	if event.is_action_pressed("ui_up"):
+		scroll_delta = -scroll_increment
+	elif event.is_action_pressed("ui_down"):
+		scroll_delta = scroll_increment
+	elif event.is_action_pressed("ui_page_up"):
+		scroll_delta = -scroll_page_increment
+	elif event.is_action_pressed("ui_page_down"):
+		scroll_delta = scroll_page_increment
+
+	if scroll_delta != 0:
+		var scroll_destination := scroll_container.scroll_vertical + scroll_delta
+		if animate:
+			var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+			tween.tween_property(scroll_container, "scroll_vertical", scroll_destination, transition_time)
+		else:
+			scroll_container.scroll_vertical = scroll_destination
 
 func load_text() -> String:
 	if credits_file_path == null or credits_file_path.is_empty():
@@ -40,7 +71,8 @@ func load_text() -> String:
 
 # Called from outside initializes the options menu
 func on_open():
-	$MarginContainer/ScrollContainer.scroll_vertical = 0.0
+	scroll_container.scroll_vertical = 0.0
+	back_button.grab_focus()
 
 
 # Emits close signal and saves the options
