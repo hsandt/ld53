71709e449be84757837271303302b7adf25f76a4
huulong <n.huu.long@gmail.com>
2023-05-16 15:25:31 +0200

    [Result] Fix Replay button not disabling buttons like Quit to main menu, allowing button spam

--- scripts/menu/ResultPanel.gd
+++ scripts/menu/ResultPanel.gd
@@ -20,6 +20,11 @@ func _ready():
 
 	var powder_stats = GameManager.powder_stats
 
+	if powder_stats.is_empty():
+		push_warning("[ResultPanel] powder_stats is empty, we must be testing Result Scene directly. ",
+			"Filling with fallback stats for testing")
+		powder_stats = [9, 99]
+
 	time_value_label.text = "%.1f" % GameManager.final_racing_time
 	powder_types_left_value_label.text = str(powder_stats[0])
 	powder_left_value_label.text = str(powder_stats[1])
@@ -38,6 +43,9 @@ func _enable_all_buttons():
 
 
 func _on_replay_button_pressed():
+	# Disable all buttons before starting fading in for safety
+	_disable_all_buttons()
+
 	GameManager.start_level()
 
 
