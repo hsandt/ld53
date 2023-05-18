36a0e8f2cbae82966290b8dd84de86ef00214cbb
huulong <n.huu.long@gmail.com>
2023-05-18 18:45:04 +0200

    [FX] OneShotFX: clear sprite at end of animation
    This now works for all FX, so removed empty frame at the end of sprite frames that had one

--- scripts/ingame/fx/OneShotFX.gd
+++ scripts/ingame/fx/OneShotFX.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var sfx_player: AudioStreamPlayer = $AudioStreamPlayer
 
 
@@ -10,10 +11,13 @@ func _ready():
 func _play_sfx():
 	sfx_player.play()
 
+
+func _on_animated_sprite_2d_animation_finished():
+	# Clear sprite at end of animation to avoid last sprite staying frozen
+	animated_sprite.visible = false
+
+
 # Set time before disappearing on FreeTimer
 # It must be at least as long as the SFX and the FX animation
-# Note that we don't react to animation_finished at the moment
-# so we don't auto-clear the last sprite, so we must add an empty frame
-# at the end of each spriteframe animation
 func _on_free_timer_timeout():
 	queue_free()
