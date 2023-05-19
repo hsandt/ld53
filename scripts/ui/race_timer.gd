94d7a15d0b750a6d0663ed0459f808c05e7da421
huulong <n.huu.long@gmail.com>
2023-05-19 22:18:16 +0200

    [Code] Fix warnings

--- scripts/ui/race_timer.gd
+++ scripts/ui/race_timer.gd
@@ -17,7 +17,7 @@ func _process(_delta):
 	var racing_time_left = in_game_manager.racing_time_left
 
 	# Text format
-	time_label.text = time_to_format(racing_time_left)
+	time_label.text = RaceTimer.time_to_format(racing_time_left)
 
 	# Color
 	if racing_time_left < 0.0:
@@ -49,7 +49,7 @@ static func split_time_in_sign_minutes_seconds(racing_time_left: float):
 	# Equivalent to `abs_floored_racing_time_left - 60 * abs_minutes`
 	var abs_seconds = abs_floored_racing_time_left % 60
 
-	return [signi(racing_time_left), abs_minutes, abs_seconds]
+	return [signf(racing_time_left), abs_minutes, abs_seconds]
 
 
 static func time_to_format(racing_time_left: float):
