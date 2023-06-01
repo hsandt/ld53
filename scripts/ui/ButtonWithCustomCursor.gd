6f16134ac67c31e142c4553f9027de571c8b0e05
huulong <n.huu.long@gmail.com>
2023-06-02 00:00:11 +0200

    [SFX] Replace more buttons with Main Menu buttons
    Only try to play button click if sfx_click_player has been set, to avoid errors on powder panel button

--- scripts/ui/ButtonWithCustomCursor.gd
+++ scripts/ui/ButtonWithCustomCursor.gd
@@ -2,7 +2,7 @@ class_name ButtonWithCustomCursor
 extends Button
 
 
-@onready var sfx_click_player: AudioStreamPlayer = $SFXClickPlayer
+@export var sfx_click_player: AudioStreamPlayer
 
 
 func _ready():
@@ -20,4 +20,5 @@ func _update_mouse_default_cursor_shape():
 	mouse_default_cursor_shape = Control.CURSOR_ARROW if disabled else Control.CURSOR_POINTING_HAND
 
 func _on_pressed():
-	sfx_click_player.play()
+	if sfx_click_player != null:
+		sfx_click_player.play()
